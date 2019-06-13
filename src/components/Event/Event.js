4e25bf07a21480ea3d332e5b88d7f72240fcb6ab
Chris Varner <varnerchris@me.com>
2019-06-13 16:46:19 -0500

    changed parser

--- src/components/Event/Event.js
+++ src/components/Event/Event.js
@@ -2,22 +2,9 @@ import React from 'react'
 import './Event.css'
 
 
- class Event extends React.Component{
-
+class Event extends React.Component{
 render(){
-  let Parser = require('rss-parser');
-  let parser = new Parser();
 
-  (async () => {
-    const CORS_PROXY = "https://cors-anywhere.herokuapp.com/"
-    let feed = await parser.parseURL(CORS_PROXY + 'https://www.winpark.org/events/feed/');
-
-    feed.items.forEach(item => {
-
-  console.log(item.title + ':' + item.pubDate)
-    });
-
-  })();
   return(
     <li><h2>Event Name </h2>
     <span>Date, Location</span>
--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -3,15 +3,29 @@ import './Menu.css'
 import Event from '../Event/Event';
 
 
- class Menu extends React.Component{
+class Menu extends React.Component{
+
 constructor(props){
   super(props);
-  this.state={
-
-  }
-  
+  this.state={  }
 }
+
+
 render(){
+  let Parser = require('rss-parser');
+  let parser = new Parser();
+
+  (async () => {
+    const CORS_PROXY = "https://cors-anywhere.herokuapp.com/"
+    let feed = await parser.parseURL(CORS_PROXY + 'https://www.winpark.org/events/feed/');
+
+    feed.items.forEach(item => {
+
+  console.log(item.title + ':' + item.pubDate)
+    });
+
+  })();
+
   return(
     <div className="events">
 
